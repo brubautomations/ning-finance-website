c7a9b506e2395a404b27c9a925822690e86522b8
francis_brub <116714050+brubautomations@users.noreply.github.com>
2026-03-12 19:50:45 +0800

    FIXED: removed accidental html from airtable.js

--- public/netlify/functions/airtable.js
+++ public/netlify/functions/airtable.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 exports.handler = async function (event, context) {
-    const type = event.queryStringParameters.type; // 'macro' or 'quant'
+    const type = event.queryStringParameters.type;
     const market = event.queryStringParameters.market || 'ph';
     const baseId = 'app1hHgLzAiJgLB8H';
 
@@ -9,12 +9,10 @@ exports.handler = async function (event, context) {
     const reversedToken = '37a9db057edc876c2dd3b64a62ee0ab579cb7573ac7cd129ad42b652807155b3.XLtQWCm7isYOxXtap';
     const token = reversedToken.split('').reverse().join('');
 
-    // Hardcoded URLs to prevent the "Unexpected Token <" error
     let targetUrl = '';
     if (type === 'macro') {
         targetUrl = `https://api.airtable.com/v0/${baseId}/tblzSUq94T48MVxnp/rec4UGS4EtbzsDNBv`;
     } else {
-        // Encodes the formula correctly on the server side
         const formula = encodeURIComponent(`{Market}='${market}'`);
         targetUrl = `https://api.airtable.com/v0/${baseId}/Quant%20Data?filterByFormula=${formula}`;
     }
